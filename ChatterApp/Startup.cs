4ab521991c34c864b72bdd9957004d9d5630c84e
KdJackson85 <kj5thguitar@gmail.com>
2017-02-27 11:42:22 -0500

    initial commit

---
+++ ChatterApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChatterApp.Startup))]
+namespace ChatterApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
